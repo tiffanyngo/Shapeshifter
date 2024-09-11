542d8427b423a76a334886798aa473c80206e365
Tiffany Ngo <tiffanyngo716@gmail.com>
2024-09-10 22:11:13 -0400

    Fix requirements.txt and update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,6 +78,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+if ENVIRONMENT == 'production':
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
